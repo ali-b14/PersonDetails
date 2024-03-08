c59d296e7073d3eea5b893b82c62adae563661b0
ali-b14 <alib714@gmail.com>
2024-03-08 15:16:57 -0500

    Added answers to comment questions

--- src/main/java/com/zipcodewilmington/Person.java
+++ src/main/java/com/zipcodewilmington/Person.java
@@ -12,16 +12,18 @@ public class Person {
         this.lastName = lastName;
     }
 
-    // why is this called a GETTER??
+    // why is this called a GETTER?? When this method is called it retrieves whatever value is stored in firstname.
     public String getFirstName() {
+
         return firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
-    @Override // why is Override here??
+    @Override // why is Override here?? the toString method would originally just return you the address. overriding it will allow you to return the values stored.
     public String toString() {
         return new StringBuilder()
                 .append("\nMy first name is " + firstName)
