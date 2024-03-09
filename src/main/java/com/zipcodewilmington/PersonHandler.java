5c008b5216bc22c53dda5a9c96f3c4c1026a042b
ali-b14 <alib714@gmail.com>
2024-03-08 20:58:32 -0500

    Completed while and for loop

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -16,17 +16,27 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // create a `counter`
-        // while `counter` is less than length of array
-            // begin loop
+        int counter = 0;
+        String currentPerson;
+        String representation;
 
+
+        // while `counter` is less than length of array // begin loop
+        while(counter < personArray.length){
+          representation = String.valueOf(personArray[counter]);
+//            System.out.println("Representation is this: " + representation);
+          result = result + representation;
+          counter++;
+            System.out.println(result);
+        }
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
 
             // end loop
+//        System.out.println("Hello" + currentPerson);
         return result;
     }
-
 
 
     public String forLoop() {
@@ -34,6 +44,15 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
+        int counter;
+        String representation;
+        String currentPerson;
+
+        for(counter = 0; counter < personArray.length; counter++){
+            representation = String.valueOf(personArray[counter]);
+            result = result + representation;
+            System.out.println(result);
+        }
 
         // use the above clauses to declare for-loop signature
             // begin loop
